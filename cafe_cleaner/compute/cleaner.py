2d4ca4933831cc4ec2035f687650394fe3f87bda
Daryl Walleck <daryl.walleck@RACKSPACE.COM>
2013-12-15 22:05:22 -0600

    Added very basic cleanup utility. Don't fear the reaper

---
+++ cafe_cleaner/compute/cleaner.py
@@ -0,0 +1,110 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import argparse
+from cafe.configurator.managers import TestEnvManager
+
+from cloudcafe.compute.config import ComputeEndpointConfig, \
+    MarshallingConfig
+from cloudcafe.compute.common.exception_handler import ExceptionHandler
+from cloudcafe.compute.flavors_api.client import FlavorsClient
+from cloudcafe.compute.servers_api.client import ServersClient
+from cloudcafe.compute.images_api.client import ImagesClient
+from cloudcafe.compute.extensions.keypairs_api.client import KeypairsClient
+from cloudcafe.auth.config import UserAuthConfig, UserConfig
+from cloudcafe.auth.provider import AuthProvider
+
+
+def entry_point():
+
+    # Set up arguments
+    argparser = argparse.ArgumentParser(prog='cafe-cleaner')
+
+    argparser.add_argument(
+        "product",
+        nargs=1,
+        metavar="<product>",
+        help="Product name")
+
+    argparser.add_argument(
+        "config",
+        nargs=1,
+        metavar="<config_file>",
+        help="Product test config")
+
+    args = argparser.parse_args()
+    config = str(args.config[0])
+    product = str(args.product[0])
+
+    test_env_manager = TestEnvManager(product, config)
+    test_env_manager.finalize()
+    compute_cleanup()
+    exit(0)
+
+
+def compute_cleanup():
+    # Load necessary configurations
+    compute_endpoint = ComputeEndpointConfig()
+    marshalling = MarshallingConfig()
+
+    endpoint_config = UserAuthConfig()
+    user_config = UserConfig()
+    access_data = AuthProvider.get_access_data(
+        endpoint_config, user_config)
+
+    # If authentication fails, halt
+    if access_data is None:
+        raise Exception('Authentication failed.')
+
+    compute_service = access_data.get_service(
+        compute_endpoint.compute_endpoint_name)
+    url = compute_service.get_endpoint(
+        compute_endpoint.region).public_url
+    # If a url override was provided, use that value instead
+    if compute_endpoint.compute_endpoint_url:
+        url = '{0}/{1}'.format(compute_endpoint.compute_endpoint_url,
+                               user_config.tenant_id)
+
+    client_args = {'url': url, 'auth_token': access_data.token.id_,
+                   'serialize_format': marshalling.serializer,
+                   'deserialize_format': marshalling.deserializer}
+
+    flavors_client = FlavorsClient(**client_args)
+    servers_client = ServersClient(**client_args)
+    images_client = ImagesClient(**client_args)
+    keypairs_client = KeypairsClient(**client_args)
+    flavors_client.add_exception_handler(ExceptionHandler())
+
+
+    keys = keypairs_client.list_keypairs().entity
+    print 'Preparing to delete {count} keys...'.format(count=len(keys))
+    for key in keys:
+        try:
+            keypairs_client.delete_keypair(key.name)
+        except:
+            print 'Failed to delete key {id}.'.format(id=key.id)
+
+    servers = servers_client.list_servers_with_detail().entity
+    print 'Preparing to delete {count} servers...'.format(count=len(servers))
+    for server in servers:
+        try:
+            servers_client.delete_server(server.id)
+        except:
+            print 'Failed to delete server {id}.'.format(id=server.id)
+
+
+if __name__ == '__main__':
+    entry_point()
---
+++ setup.py
@@ -0,0 +1,61 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.install import install as _install
+except ImportError:
+    #currently broken, this really only works with setuptools
+    from distutils.core import setup, find_packages
+    from distutils.command.install import install as _install
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+#Normal setup stuff
+setup(
+    name='cafe-cleaner',
+    version='0.1.0',
+    description='Cleans up all the things.',
+    long_description='{0}\n\n{1}'.format(
+        open('README.md').read(),
+        open('HISTORY.md').read()),
+    author='Rackspace Cloud QE',
+    author_email='cloud-cafe@lists.rackspace.com',
+    url='http://rackspace.com',
+    packages=find_packages(),
+    package_data={'': ['LICENSE', 'NOTICE']},
+    package_dir={'cafe': 'cafe'},
+    include_package_data=True,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+    entry_points = {
+        'console_scripts':
+        ['cafe-cleaner = cafe_cleaner.compute.cleaner:entry_point']})
